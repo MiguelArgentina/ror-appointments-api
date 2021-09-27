ccb158903ad5e0325c0fc8e237a642ce71acf188
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-09-27 13:36:37 -0300

    Add roles to users (admin, user, provider). Default role is user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::API
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   rescue_from ActionController::RoutingError, :with => :error_render_method
 
+  
 
 
   def route_not_found
     render json: { error: "Please verify the enpoint. It wasn´t found in our service."}, status: :not_found
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
+  enum role: [:user, :provider, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
   devise :database_authenticatable,
           :jwt_authenticatable,
           :registerable,
           jwt_revocation_strategy: JwtDenylist
+
+  private
+
+  def set_default_role
+    self.role ||= :user
+  end
+
 end
